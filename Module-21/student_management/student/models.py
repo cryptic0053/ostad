df67eeab5c2778174e341cbe6b95dd2928cd1b68
cryptic0053 <anirbanargha0053@gmail.com>
2025-02-19 22:38:19 +0600

    21

--- Module-21/student_management/student/models.py
+++ Module-21/student_management/student/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    password = models.CharField(max_length=10)
+    checkbox = models.BooleanField
+
+    def __str__(self):
+        return f"{self.name}"
--- Module-21/student_management/student/views.py
+++ Module-21/student_management/student/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from . import models
 
 # Create your views here.
 def home(request):
-    return render(request,'student/index.html',)
+    print(request.POST)  # Debugging to see received data
+
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        password = request.POST.get('password')
+        
+        student = models.Student(name=name, email=email, phone=phone, password=password)
+        student.save()
+        
+        return HttpResponse("Student Object Created Successfully")
+
+    return render(request, 'student/index.html')

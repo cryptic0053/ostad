8f5f6f6ee2f4559beebc55b2d27a21c8e419ab27
cryptic0053 <anirbanargha0053@gmail.com>
2025-04-26 21:59:53 +0600

    rest-api

---
+++ Module-29/contactapp/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import Contact
+
+class ContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contact
+        fields = "__all__"
+        extra_kwargs = {
+            "name":{
+               "min_length":3,
+               "max_length":10,
+               "error_messages":{"min_length":"Name must be 3 char long"},
+               "error_messages":{"max_length":"Name must not be more than 10 char long"},
+
+            },
+            #"address":{"required":False}
+        }
